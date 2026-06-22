bff86c1697c3c60ab8cad68da72a839ea8776ba4
Ann Kariuki <awk19@duke.edu>
2026-06-22 08:17:25 -0700

    bad request status code for creating users without all fields

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,5 +1,14 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
 const register = async (req, res) => {
-    res.send('register user')
+    // const {name, email, password} = req.body
+    // if(!name || !email || !password){
+    //     throw new BadRequestError('Please provide name, email and password')
+    // }
+    //we could use this above but right now we are using the schema validation to make sure the user has all the correct fields 
+    const user = await User.create({...req.body})
+    res.status(StatusCodes.CREATED).json(req.body)
 }
 
 const login = async (req, res) => {
