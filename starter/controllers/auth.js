0182a776f8822d76cebc6a209d73e446b483850e
Ann Kariuki <ann.kariuki@duke.edu>
2026-06-27 19:06:02 -0700

    setup login controller

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 const register = async (req, res) => {
     // const {name, email, password} = req.body
@@ -8,13 +8,24 @@ const register = async (req, res) => {
     //     throw new BadRequestError('Please provide name, email and password')
     // }
     //we could use this above but right now we are using the schema validation to make sure the user has all the correct fields 
-    const user = await User.create({...req.body})
+    const user = await User.create({ ...req.body })
     const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({ email })
+    //compare password
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
