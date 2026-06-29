3284c0b212b9888e00b276df6eba950adfbf4841
Ann Kariuki <ann.kariuki@duke.edu>
2026-06-28 17:59:16 -0700

    get all jobs controller

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -1,8 +1,11 @@
 const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
+
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
+    //find jobs for one user only
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
@@ -10,10 +13,9 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-    console.log(req.user)
     req.body.createdBy = req.user.userId
     const job = await Job.create(req.body)
-    res.status(StatusCodes.CREATED).json({job})
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
