2470f72d604d515f405a8d02f6fceeda11667b38
Ann Kariuki <ann.kariuki@duke.edu>
2026-06-29 22:40:39 -0700

    update job controller

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -9,11 +9,11 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-    const {user:{userId},params:{id:jobId}} = req
+    const { user: { userId }, params: { id: jobId } } = req
     const job = await Job.findOne({
-        _id:jobId, createdBy:userId
+        _id: jobId, createdBy: userId
     })
-    if(!job){
+    if (!job) {
         throw new NotFoundError(`No job with id ${jobId}`)
     }
     res.status(StatusCodes.OK).json({ job })
@@ -26,7 +26,16 @@ const createJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-    res.send('update jobs')
+    const { body: { company, position }, user: { userId }, params: { id: jobId } } = req
+    if (company === '' || position === '') {
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+
+    const job = await Job.findByIdAndUpdate({ _id: jobId, createdBy: userId }, req.body, { new: true, runValidators: true })
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
